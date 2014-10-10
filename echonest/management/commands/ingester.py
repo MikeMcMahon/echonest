a876e7ae7f985d91e8f7c660404b938b666075df
Mike McMahon <mike@mcmahon.com>
2014-10-09 18:53:37 -0700

    initial project commit

---
+++ echonest/management/commands/ingester.py
@@ -0,0 +1,39 @@
+from django.core.management import BaseCommand
+
+import os
+import shutil
+import datetime
+
+import sys
+from echonest import settings
+
+sys.path.insert(0, os.path.join(settings.INGESTER_API_DIR, 'API'))
+sys.path.insert(0, os.path.join(settings.INGESTER_API_DIR, 'util'))
+
+import fastingest
+import fp
+
+
+class Command(BaseCommand):
+
+    def backup(self, files):
+        stamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%MZ')
+
+        for f in files:
+            dst = os.path.join(settings.INGESTER_BACK_DIR, "{0}-{1}".format(stamp, f))
+            src = os.path.join(settings.INGESTER_JSON_DIR, f)
+            print "Moving {0} to {1}".format(src,  dst)
+            shutil.move(src, dst)
+
+    def handle(self, *args, **options):
+        files = os.listdir(settings.INGESTER_JSON_DIR)
+        files = [f for f in files if '.json' in f]
+
+        for f in files:
+            print "Ingesting file {0}".format(f)
+            codes, bigeval = parse_json_dump(f)
+            fp.ingest(codes, do_commit=False)
+        else:
+            print "Commiting to database!"
+            fp.commit()
+            self.backup(files)
