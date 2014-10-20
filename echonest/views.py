4b4866b1d22b3a3b838cc00fd31381d717bee77f
Mike McMahon <mike@mcmahon.com>
2014-10-20 14:03:50 -0700

    ordering

--- echonest/views.py
+++ echonest/views.py
@@ -80,7 +80,7 @@ def song_listing(request, reason):
         match = False
         title = 'Unmatched Track Information'
 
-    ingested = Ingested.objects.filter(match=match)
+    ingested = Ingested.objects.filter(match=match).orderby('uploaded_on')
 
     return render(request, 'songlisting.html', {
         'title': title,
