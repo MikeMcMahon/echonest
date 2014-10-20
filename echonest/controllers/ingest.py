f7496304be1527c559373c8d525af283f6fb16cd
Mike McMahon <mike@mcmahon.com>
2014-10-20 14:02:38 -0700

    don't retry forever

--- echonest/controllers/ingest.py
+++ echonest/controllers/ingest.py
@@ -8,15 +8,17 @@ import time
 from echonest import settings
 
 
-def process(ingest):
+def process(ingest, retry=0):
     json_data = {'track_id': None}
     if settings.REMOTE_ENABLED:
         try:
             scraped = urllib2.urlopen(settings.REMOTE_API_URL + ingest.code)
         except urllib2.URLError:
             time.sleep(5)
-            return process(ingest)
-
-        json_data = json.loads(scraped.read())
+            retry += 1
+            if retry <= 3:
+                return process(ingest, retry)
+        else:
+            json_data = json.loads(scraped.read())
 
     return json_data['track_id']
