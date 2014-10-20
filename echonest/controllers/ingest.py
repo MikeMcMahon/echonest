aca11ef09a2e96b99b1909e3f581bd4fc6318d96
Mike McMahon <mike@mcmahon.com>
2014-10-20 14:00:29 -0700

    if we can't hit the url sleep the server for five seconds and try again

--- echonest/controllers/ingest.py
+++ echonest/controllers/ingest.py
@@ -4,13 +4,19 @@ date: 10/20/2014
 """
 import json
 import urllib2
+import time
 from echonest import settings
 
 
 def process(ingest):
     json_data = {'track_id': None}
     if settings.REMOTE_ENABLED:
-        scraped = urllib2.urlopen(settings.REMOTE_API_URL + ingest.code)
+        try:
+            scraped = urllib2.urlopen(settings.REMOTE_API_URL + ingest.code)
+        except urllib2.URLError:
+            time.sleep(5)
+            return process(ingest)
+
         json_data = json.loads(scraped.read())
 
     return json_data['track_id']
