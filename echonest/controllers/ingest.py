93eceaa73b48af878f9972d1d317bc0901bd882a
Mike McMahon <mike@mcmahon.com>
2014-11-14 21:58:46 -0800

    always use remote

--- echonest/controllers/ingest.py
+++ echonest/controllers/ingest.py
@@ -13,19 +13,19 @@ import fp
 
 def process(ingest, retry=0):
     json_data = {'track_id': None}
-    if settings.REMOTE_ENABLED:
-        try:
-            scraped = urllib2.urlopen(settings.REMOTE_API_URL + ingest.code)
-        except urllib2.URLError:
-            time.sleep(5)
-            retry += 1
-            if retry <= 3:
-                return process(ingest, retry)
-        else:
-            json_data = json.loads(scraped.read())
+    #if settings.REMOTE_ENABLED:
+    try:
+        scraped = urllib2.urlopen(settings.REMOTE_API_URL + ingest.code)
+    except urllib2.URLError:
+        time.sleep(5)
+        retry += 1
+        if retry <= 3:
+            return process(ingest, retry)
     else:
-        response = fp.best_match_for_query(ingest.code)
-        json_data['track_id'] = response.TRID
+        json_data = json.loads(scraped.read())
+    #else:
+    #    response = fp.best_match_for_query(ingest.code)
+    #    json_data['track_id'] = response.TRID
 
     return json_data['track_id']
 
